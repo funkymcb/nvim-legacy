2bf3d4a264c99ff92299d7de9d2276add3e6ac64
Yannick Peter <yannick.peter@fino.digital>
2022-09-13 00:20:24 +0200

    disable nvim-tree close if last buffer

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -17,12 +17,13 @@ require("nvim-tree").setup({
 })
 require("nvim-web-devicons").setup()
 
+-- TODO fix this since it causes vim to crash if nvim-tree is up and telescope tries to open something
 -- close vim if nvim-tree is the last buffer
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   nested = true,
+--   callback = function()
+--     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+--       vim.cmd "quit"
+--     end
+--   end
+-- })
