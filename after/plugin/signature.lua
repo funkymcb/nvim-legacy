1d0182606e85cb44f01a6ef6d5e3961d09234f3e
Yannick Peter <yannick.peter@fino.digital>
2022-09-14 15:32:08 +0200

    some more style changes

--- after/plugin/signature.lua
+++ after/plugin/signature.lua
@@ -1,7 +1,11 @@
+vim.cmd [[highlight LspSignatureActiveCustomParameter guifg=#31C236 gui=nocombine]]
+
 -- signature setup (floating window over function completion)
-  require'lsp_signature'.setup({
-    bind = true,
-    handler_opts = {
-      border = "rounded"
-    }
-  }, bufnr)
+require'lsp_signature'.setup({
+  bind = true,
+  hint_prefix = "💡 ",
+  hi_parameter = "LspSignatureActiveCustomParameter",
+  handler_opts = {
+    border = "rounded"
+  }
+}, bufnr)
