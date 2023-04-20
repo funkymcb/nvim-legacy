d439250e15b640e114f335985bf96b4b02f5207a
Yannick Peter <yannick.peter@empiriecom.com>
2023-04-20 11:53:03 +0200

    update go autosave

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,7 +28,8 @@ lsp.configure('yamlls', {
                "ingress.yaml",
                "service.yaml",
                "namespace.yaml",
-               "hpa.yaml"
+               "hpa.yaml",
+               "release.yaml"
             },
             ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
             -- github workflows
@@ -65,4 +66,11 @@ vim.diagnostic.config({
 -- golang
 -- gofmt and goimport on save
 require("go").setup()
-vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
